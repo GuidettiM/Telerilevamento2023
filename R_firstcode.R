674bd85a7d44d5c2c29a0ce20c50785f91006daa
GuidettiM <127352157+GuidettiM@users.noreply.github.com>
2023-03-16 17:45:32 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -27,3 +27,37 @@ plot(l2011$B4_sre, col=cl)
 nir <- l2011[[4]]
 plot(nir, col=cl)
 
+# Exercise: using strange colours
+c2 <- colorRampPalette(c("cyan", "azure", "darkorchid", "aquamarine")) (100)
+plot(l2011, col=c2)
+# Exercise: plot only the infrared band
+plot(l2011[[4]], col=c2)
+# or
+nir <- l2011$B4_sre
+plot(nir, col=c2)
+
+# Export graphs in R
+pdf("myfirstgraph.pdf") #creates a pdf file with our plot
+plot(nir, col=c2)
+dev.off() #closes the window
+
+#Plotting several graphs all at once: multiframe
+par(mfrow=c(2,1))
+plot(l2011[[3]], col=c2)
+plot(l2011[[4]], col=c2)
+
+# Plotting 4 plots in a 2 * 2 multiframe. Each plot with a different colour palette
+par(mfrow=c(2,2))
+#blue
+clb <- colorRampPalette(c("blue4", "blue2", "light blue")) (100)
+plot(l2011[[1]], col=clb)
+#green
+clg <- colorRampPalette(c("chartreuse4", "chartreuse2", "chartreuse")) (100)
+plot(l2011[[2]], col=clg)
+#rosso
+clbr <- colorRampPalette(c("brown4", "brown2", "brown1")) (100)
+plot(l2011[[3]], col=clbr)
+#infrarosso
+clo <- colorRampPalette(c("darkorchid4", "darkorchid2", "aquamarine")) (100)
+plot(l2011[[4]], col=clo)
+dev.off() # closes the window.
