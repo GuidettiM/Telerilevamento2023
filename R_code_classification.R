21b99f0dc18cf6b7ccd5ebfd1d543a5baffa6fb7
GuidettiM <127352157+GuidettiM@users.noreply.github.com>
2023-04-13 17:46:39 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -3,7 +3,15 @@
 #Installing devtools
 #install.packages("devtools")
 
-library(devtools)
+#library(devtools)
 
 #devtools::install_github("bleutner/RStoolbox") #installing a package from Benjamin Bleutner GitHUb
 #library(RStoolbox)
+
+library(raster)
+
+setwd("C:/lab/")
+
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+plotRGB(so, 1, 2, 3, stretch="lin")
