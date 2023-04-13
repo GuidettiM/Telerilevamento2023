726b71e4486197877a797f0a30c3c48f4cc14dc7
GuidettiM <127352157+GuidettiM@users.noreply.github.com>
2023-04-13 17:23:47 +0200

    Create R_code_classification.R

---
+++ R_code_classification.R
@@ -0,0 +1,9 @@
+# Classification of remote sensing data via RStoolbox
+
+#Installing devtools
+#install.packages("devtools")
+
+library(devtools)
+
+#devtools::install_github("bleutner/RStoolbox") #installing a package from Benjamin Bleutner GitHUb
+#library(RStoolbox)
