97fbc93ad72725bc90ba4a2c31f95d16893a5ecd
GuidettiM <127352157+GuidettiM@users.noreply.github.com>
2023-05-11 17:48:38 +0200

    Create R_code_Copernicus.r

---
+++ R_code_Copernicus.r
@@ -0,0 +1,11 @@
+# R code for downloading and visualising Copernicus data
+
+# install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+
+setwd("C:/lab/")
+
+# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+# dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
